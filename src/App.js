308804615bc20d9aedf0a338c313786b273d79b6
Michael Nazareth <mcnazareth@gmail.com>
2021-09-09 16:25:06 -0400

    Michaels misery

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -6,6 +6,8 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
   const ShowFoods = () => {
     return foods.map((foodItem) => {
       return <FoodBox food={foodItem} />;
@@ -14,6 +16,15 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={() => setShowForm(!showForm)}>Add New Foods</button>
+      {showForm ? (
+        <form>
+          <input placeholder="Add Food"></input>
+          <button>Add Food</button>
+        </form>
+      ) : (
+        'Not Showing.'
+      )}
       <ShowFoods />
     </div>
   );
